61ecca3330a0b38ed99c601cd4759787ed3af8a4
”0xAureoleNode“ <aureolenode@gmail.com>
2022-12-02 16:02:41 +0800

    added Registed Func

--- src/components/auth/AuthContainer.js
+++ src/components/auth/AuthContainer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Login from './Login';
+import Register from './Register';
 import './AuthContainer.scss';
 
 const AuthContainer = () => {
   return (
     <section className="--flex-center --100vh">
       <div className="container box">
-        <Login />
+        {/* <Login /> */}
+        <Register />
       </div>
     </section>
   );
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import registerImg from '../../assets/register.svg';
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="main-container --flex-center">
+      <div className="form-container">
+        <form className="--form-control">
+          <h2 className="--color-danger --text-center">Register</h2>
+          <input type="text" className="--width-100" placeholder="Username" />
+          <input type="email" className="--width-100" placeholder="Email" />
+          <input
+            type="password"
+            className="--width-100"
+            placeholder="Password"
+          />
+          <button className="--btn --btn-primary --btn-block">Register</button>
+
+          <span className="--text-sm --block">
+            Have an account?
+            <a href="#" className="--text-sm">
+              Login
+            </a>
+          </span>
+        </form>
+      </div>
+      <div className="img-container">
+        <img src={registerImg} alt="login-img" />
+      </div>
+    </div>
+  );
 };
 
 export default Register;
